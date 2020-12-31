8392be184acd3b9b36bd565dab30bb39c701b0f5
gustavohrg <gustavohrg@live.com>
2020-12-31 10:44:41 -0300

    fixing bug @redirect-post-product

--- controllers/admin.js
+++ controllers/admin.js
@@ -22,6 +22,7 @@ exports.postAddProduct = (req, res, next) => {
     .then((result) => {
       // console.log(result);
       console.log('Inserido com sucesso');
+      res.redirect('/admin/products');
     })
     .catch((err) => console.log(err));
 };
