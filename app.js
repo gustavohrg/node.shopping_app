45cd0396628b390afab299b5afa573920bc3e387
gustavohrg <gustavohrg@live.com>
2020-12-31 11:53:59 -0300

    creating and fetching a cart

--- app.js
+++ app.js
@@ -45,8 +45,8 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  .sync({ force: true })
-  // .sync()
+  // .sync({ force: true })
+  .sync()
   .then((result) => {
     // console.log(result);
     return User.findByPk(1);
@@ -59,6 +59,9 @@ sequelize
   })
   .then((user) => {
     // console.log(user);
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(3000);
   })
   .catch((err) => console.log(err));
